bbc3b012500fd74cd15fa773b26ff0f85289549f
Marcos Coca <mcoca@evopointsolution.com>
2021-09-24 18:50:40 -0400

    feat(config): add code and commits linter

--- src/components/atoms/Button/Button.tsx
+++ src/components/atoms/Button/Button.tsx
@@ -29,6 +29,7 @@ export interface ButtonProps {
  */
 export const Button = ({ primary = false, size = "medium", backgroundColor, label, ...props }: ButtonProps) => {
   const mode = primary ? "storybook-button--primary" : "storybook-button--secondary";
+
   return (
     <button
       type="button"
